113eb88d6bb5222fac6e69d05815441813e96f68
Maciej Kędra <maciejken@gmail.com>
2022-04-28 12:12:47 +0200

    NORTHSTAR-5: add days of week

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, { FC } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import WeekList from './WeekList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <WeekList />
     </div>
   );
 }
